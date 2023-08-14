ef1c840e6d914f5afb37dbf9d3eee5aa0b09c405
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-08-14 15:36:51 +0330

    add feature

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import "./bootstrap.min.css"
+import './App.css'
 import Header from './Header/Header';
 import About from './About/About';
-import './App.css'
+import Client from "./Client/Client";
+import Features from "./Features/Features";
 
 function App() {
   return (
     <div className="App">
      <Header />
      <About />
+     <Client />
+     <Features />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
+     <br />
     </div>
   );
 }
