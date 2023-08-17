7aa9bb12dcd640c6a436b6adf35092f4e87490fb
hanie ghorbani <hnie.ghorbani@gmail.com>
2023-08-17 11:58:55 +0330

    add details

--- src/Header/Boxs/Box.js
+++ src/Header/Boxs/Box.js
@@ -1,11 +1,11 @@
 import './Box.css'
 import "../../bootstrap-icons.min.css";
 
-export default function Box() {
+export default function Box({title,icon}) {
   return (
     <div className='border text-center fs-5 box-header mb-2'>
-        <i className='bi bi-calendar text-warning fw-bold'></i>
-        <p className='text-white fw-bold'>منابع و اطلاعات</p>
+        <i className='bi bi-calendar  text-warning fw-bold'></i>
+        <p className='text-white fw-bold'>{title}</p>
     </div>
   )
 }
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -14,11 +14,11 @@ export default function Header() {
       </div>
 
       <div className="d-flex mx-auto w-75 justify-content-between mb-5 boxs-header">
-        <Box />
-        <Box />
-        <Box />
-        <Box />
-        <Box />
+        <Box title="منابع و اطلاعات" icon='bi bi-graph-up'/>
+        <Box title="متن نمایشی" icon='bi bi-menu-button-wide-fill '/>
+        <Box title="نمودار کیفی" icon='bi bi-bar-chart-line-fill'/>
+        <Box title="نمونه کارها" icon='bi bi-grid-1x2-fill'/>
+        <Box title="اطلاعات ذخیره شده" icon='bi bi-calendar '/>
       </div>
     </div>
   )
